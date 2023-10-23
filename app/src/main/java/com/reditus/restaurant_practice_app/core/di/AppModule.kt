c4c38d10824b1744a2924b5b00611522b03b0b72
Hong <bayyyy@naver.com>
2023-10-24 01:05:47 +0900

    [Feat]: refresh Token 임시 추가

--- app/src/main/java/com/reditus/restaurant_practice_app/core/di/AppModule.kt
+++ app/src/main/java/com/reditus/restaurant_practice_app/core/di/AppModule.kt
@@ -3,15 +3,17 @@ package com.reditus.restaurant_practice_app.core.di
 import android.content.Context
 import android.content.SharedPreferences
 import android.util.Log
+import com.google.gson.Gson
 import com.reditus.restaurant_practice_app.data.api.ApiService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import kotlinx.coroutines.runBlocking
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
 import okhttp3.Response
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Converter
@@ -19,6 +21,8 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Inject
 import javax.inject.Singleton
+
+
 
 class HeaderInterceptor @Inject constructor(
     private val prefs: SharedPreferences
@@ -39,7 +43,46 @@ class HeaderInterceptor @Inject constructor(
         val newRequest = chain.request().newBuilder()
             .addHeader("Authorization", token)
             .build()
-        return chain.proceed(newRequest)
+        val response = chain.proceed(newRequest)
+
+        //accessToken이 만료되었을 때
+        if(response.code == 401){
+            response.close()
+            val x = HttpLoggingInterceptor()
+                .setLevel(HttpLoggingInterceptor.Level.BODY)
+            val okHttp = OkHttpClient.Builder().addInterceptor(x).build()
+
+            val refreshToken = runBlocking {
+                prefs.getString("refreshToken", "")
+            }
+
+            val refreshRequest = Request.Builder()
+                .url("https://restaurant-app.run.goorm.site/auth/token")
+                .post("".toRequestBody())
+                .addHeader("authorization", "Bearer ${refreshToken!!}")
+                .build()
+
+            val refreshResponse = okHttp.newCall(refreshRequest).execute()
+
+            if(refreshResponse.code == 201){
+                val gson = Gson()
+                val refreshResponseJson = gson.fromJson(refreshResponse.body?.string(), Map::class.java)
+                val newAccessToken = refreshResponseJson["accessToken"].toString()
+                prefs.edit().putString("accessToken", newAccessToken).apply()
+                val originRequest = chain.request().newBuilder()
+                    .removeHeader("Authorization")
+                    .addHeader("Authorization", "Bearer $newAccessToken")
+                    .build()
+                return chain.proceed(originRequest)
+            }else{
+                //TODO: 로그아웃 처리
+                prefs.edit().remove("accessToken").apply()
+                prefs.edit().remove("refreshToken").apply()
+            }
+        }
+
+
+        return response
     }
 }
 
